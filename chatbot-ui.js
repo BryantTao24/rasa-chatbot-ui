fe66558f0ea3d004a19b1c34303c64989fdc243c
Kevin Tao <minzetao@gmail.com>
2021-09-28 13:36:51 +0800

    feat 🎸: Add jquery.min.js file in local project

--- chatbot-ui.js
+++ chatbot-ui.js
@@ -6,8 +6,7 @@ function init() {
   //---------------------------- Including Jquery ------------------------------
 
   var script = document.createElement("script");
-  script.src =
-    "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+  script.src = "jquery.min.js";
   script.type = "text/javascript";
   document.getElementsByTagName("head")[0].appendChild(script);
 
